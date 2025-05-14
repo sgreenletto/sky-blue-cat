8821cca950f531a87a9de1fa13433df0fa2410e6
牛若水 <15136736279@163.com>
2025-05-14 10:55:09 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
-#include "petgifwindow.h"
 #include <QApplication>
+#include "views/dialogs/startdialog.h"
+#include "views/mainwindow.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
-    PetGifWindow w(Idle, nullptr); // Idle 是初始动图
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    
+    // 先创建并显示开始对话框
+    StartDialog startDialog;
+    startDialog.setModal(true);
+    
+    if (startDialog.exec() == QDialog::Accepted) {
+        // 如果用户点击了开始按钮，才创建并显示主窗口
+        MainWindow window;
+        window.initialize();
+        window.show();
+        return app.exec();
+    }
+    
+    // 如果用户点击了退出按钮，直接退出程序
+    return 0;
 }
